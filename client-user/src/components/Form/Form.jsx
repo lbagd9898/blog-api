1230bfb513917232977ee6e9e98b8a22ccbb96ee
Lydia Bagdon <74695306+lbagd9898@users.noreply.github.com>
2025-10-17 11:22:27 -0400

    array form population with state variables added

--- client-user/src/components/Form/Form.jsx
+++ client-user/src/components/Form/Form.jsx
@@ -1,8 +1,23 @@
 import styles from "./Form.module.css";
 import Button from "../Button/Button";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function Form({ fields, title, buttonText }) {
+  const [inputVal, setInputVal] = useState(() =>
+    fields.reduce((acc, field) => {
+      return { ...acc, [field.name]: "" };
+    }, {})
+  );
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setInputVal((prev) => ({ ...prev, [name]: value }));
+  }
+
+  useEffect(() => {
+    console.log("input values", inputVal);
+  }, [inputVal]);
+
   return (
     <div className={styles["form-div"]}>
       <form>
@@ -10,7 +25,12 @@ function Form({ fields, title, buttonText }) {
         {fields.map((field) => (
           <div className={styles["form-field"]}>
             <label for={field.name}>{field.label}</label>
-            <input type={field.type} />
+            <input
+              type={field.type}
+              name={field.name}
+              value={inputVal[field.name]}
+              onChange={handleChange}
+            />
           </div>
         ))}
         <Button text={buttonText}></Button>
