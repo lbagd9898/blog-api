b956ebfd81203e2519c7979f40dd800be3d80751
Lydia Bagdon <74695306+lbagd9898@users.noreply.github.com>
2025-10-29 14:00:35 -0400

    server queried for posts. Need to display on dashboard page

--- client-user/src/pages/dashboard.jsx
+++ client-user/src/pages/dashboard.jsx
@@ -1,8 +1,40 @@
 import styles from "./dashboard.module.css";
 import Header from "../components/Header/Header.jsx";
 import Post from "../components/Post/Post.jsx";
+import { useAuth } from "../utils/AuthContext.jsx";
+import { useState, useEffect } from "react";
 
 function Dashboard() {
+  const { token } = useAuth();
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    if (!token) return;
+
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/posts", {
+          headers: {
+            "Content-Type": "application.json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (!response.ok) {
+          console.log(data.message);
+          throw new Error("Failed to fetch posts");
+        }
+        const data = await response.json();
+        console.log(data);
+        setPosts(data);
+      } catch (err) {
+        console.error("Error fetching posts:", err);
+      }
+    };
+
+    fetchPosts();
+  }, [token]);
+
   return (
     <>
       <Header></Header>
